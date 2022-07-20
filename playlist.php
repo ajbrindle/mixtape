7451a95e222c7935914f95eae20fffbb01c536e2
Andrew Brindle <andrew.brindle@gmail.com>
2022-07-20 10:06:44 +0100

    Enhanced CORS checks

--- playlist.php
+++ playlist.php
@@ -118,10 +118,22 @@ $result = curl_exec($cURLConnection);
 checkStatus($cURLConnection, $result);
 curl_close($cURLConnection);
 
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+}
+    
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    exit(0);
+}
+
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST');
-header('Access-Control-Allow-Headers: *');
 echo json_encode($return);
 
 function checkStatus($c, $result) {
